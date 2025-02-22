6f7e224fc54a6fa2ac5ef78ead85bc67a4fadcf0
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-02-22 15:26:17 -0300

    projeto atualizado

--- JAVASCRIPT/funcionamento.js
+++ JAVASCRIPT/funcionamento.js
@@ -1,10 +1,14 @@
  // Seleciona todas as cadeiras
  var cadeiras = document.querySelectorAll('.cadeiras');
+ var resultado = document.getElementById('resultado')
+ var prosseguir = document.getElementById('prosseguir')
 
  // Adiciona o evento de clique a cada uma
  cadeiras.forEach(function(cadeira) {
      cadeira.addEventListener("click", function() {
          cadeira.style.backgroundColor = "yellow"; // Muda a cor ao clicar
-         
-     });
+         resultado.style.display = "block"
+         resultado.innerText = "Lugar " + cadeira.innerText + " selecionado";
+         prosseguir.style.display = "block"  
+    });
  });
